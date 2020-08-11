8915c8b1a92624b73a4739192aeb73c67e2bfd84
Van Nguyen <van@shirotech.com>
2020-08-12 00:09:13 +1000

    fixup js-base64 proper (#134)

--- src/paths.js
+++ src/paths.js
@@ -1,4 +1,5 @@
 module.exports = {
+  PATH_BASE64_JS: 'js-base64',
   PATH_CSS: './assets/styles.css',
-  PATH_DIFF_CSS: 'diff2html/dist/diff2html.css'
+  PATH_DIFF_CSS: 'diff2html/dist/diff2html.css',
 };
