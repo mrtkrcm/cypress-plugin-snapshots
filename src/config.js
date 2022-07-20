79ce9e3901f3a5fe364c7b4f55b6b3e225b37bf2
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-07-21 01:26:42 +0200

    fix: add missing options in getScreenshotConfig()

--- src/config.js
+++ src/config.js
@@ -1,15 +1,15 @@
-const { merge, cloneDeep, clone, pick } = require('lodash');
-const { COMMAND_MATCH_SNAPSHOT } = require('./commands/commandNames');
+const { merge, cloneDeep, clone, pick } = require("lodash")
+const { COMMAND_MATCH_SNAPSHOT } = require("./commands/commandNames")
 
-const CONFIG_KEY = 'cypress-plugin-snapshots';
+const CONFIG_KEY = "cypress-plugin-snapshots"
 
 const DEFAULT_IMAGE_CONFIG = Object.freeze({
   failOnSnapshotDiff: true,
   createDiffImage: true,
   resizeDevicePixelRatio: true,
   threshold: 0.1,
-  thresholdType: 'percent', // can be 'percent' or 'pixel'
-});
+  thresholdType: "percent" // can be 'percent' or 'pixel'
+})
 
 const DEFAULT_CONFIG = Object.freeze({
   autoCleanUp: false,
@@ -24,65 +24,68 @@ const DEFAULT_CONFIG = Object.freeze({
   prettier: true,
   prettierConfig: {
     html: {
-      parser: 'html',
+      parser: "html",
       tabWidth: 2,
-      endOfLine: 'lf',
-    },
+      endOfLine: "lf"
+    }
   },
   screenshotConfig: {
-    blackout: ['.snapshot-diff'],
-    log: false,
+    blackout: [".snapshot-diff"],
+    log: false
   },
   updateSnapshots: false,
-  backgroundBlend: 'difference',
-  diffFormat: 'side-by-side',
-});
+  backgroundBlend: "difference",
+  diffFormat: "side-by-side"
+})
 
-let config = cloneDeep(DEFAULT_CONFIG);
+let config = cloneDeep(DEFAULT_CONFIG)
 
 function initConfig(initialConfig) {
-  config = merge({}, DEFAULT_CONFIG, initialConfig);
+  config = merge({}, DEFAULT_CONFIG, initialConfig)
 
-  config.screenshotConfig.blackout = config.screenshotConfig.blackout || [];
-  if (!config.screenshotConfig.blackout.includes('.snapshot-diff')) {
-    config.screenshotConfig.blackout.push('.snapshot-diff');
+  config.screenshotConfig.blackout = config.screenshotConfig.blackout || []
+  if (!config.screenshotConfig.blackout.includes(".snapshot-diff")) {
+    config.screenshotConfig.blackout.push(".snapshot-diff")
   }
-  return config;
+  return config
 }
 
 function getConfig() {
-  return config;
+  return config
 }
 
 function mergeConfig(commandName, taskOptions) {
-  const options = cloneDeep(config);
+  const options = cloneDeep(config)
 
   if (commandName === COMMAND_MATCH_SNAPSHOT) {
-    merge(options, taskOptions);
-    return pick(options, Object.keys(DEFAULT_CONFIG));
+    merge(options, taskOptions)
+    return pick(options, Object.keys(DEFAULT_CONFIG))
   }
 
   const screenshotConfigOptions = [
-    'log',
-    'blackout',
-    'capture',
-    'clip',
-    'disableTimersAndAnimations',
-    'padding',
-    'scale',
-    'timeout',
-    'onBeforeScreenshot',
-    'onAfterScreenshot',
-  ];
+    "log",
+    "blackout",
+    "capture",
+    "clip",
+    "disableTimersAndAnimations",
+    "padding",
+    "scale",
+    "timeout",
+    "onBeforeScreenshot",
+    "onAfterScreenshot"
+  ]
 
-  merge(options.imageConfig, pick(taskOptions, Object.keys(DEFAULT_CONFIG.imageConfig)));
-  merge(options.screenshotConfig, pick(taskOptions, screenshotConfigOptions));
+  merge(
+    options.imageConfig,
+    pick(taskOptions, Object.keys(DEFAULT_CONFIG.imageConfig))
+  )
+  merge(options.screenshotConfig, pick(taskOptions, screenshotConfigOptions))
 
   if (taskOptions && taskOptions.name) {
-    options.name = taskOptions.name;
+    options.name = taskOptions.name
   }
 
-  return options;
+  return options
 }
 
 module.exports = {
@@ -90,5 +93,5 @@ module.exports = {
   DEFAULT_IMAGE_CONFIG,
   initConfig,
   getConfig,
-  mergeConfig,
-};
+  mergeConfig
+}
